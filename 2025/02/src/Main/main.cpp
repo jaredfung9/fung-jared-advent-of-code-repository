a2c9df30d2e48224e0d85704fc0ef54f2851b418
Jared Fung <jaredfung9@gmail.com>
2025-12-06 14:13:37 -0800

    Day 02 | used longs and range checks + pow

--- 2025/02/src/Main/main.cpp
+++ 2025/02/src/Main/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cmath>
 
 
 using std::cout;
@@ -9,6 +10,55 @@ using std::ifstream;
 using std::string;
 
 string INPUT = "inputs/input.txt";
+long generateID(int half, int exp) {
+    return half + std::pow(10, exp) * half;
+}
+
+bool isInvalidID(int ID, int low, int upper) {
+    return (ID >= low) && (ID <= upper);
+}
+
+int countDigits(int x) {
+    return std::log10(x) + 1;
+}
+
+/* Given two std::string ptrs, LOWSTR and UPPERSTR, returns the sum from adding all invalid IDS in that range together. */
+long countInvalidIDs(string* lowStr, string* upperStr) {
+    long sum = 0;
+    long low = stol((*lowStr));
+    long upper = stol((*upperStr));
+    int midpoint = lowStr->size() / 2;
+    long half = 1;
+    if ((lowStr->size() % 2) != 0) {
+        half = pow(10,midpoint);
+        midpoint++;
+    } else {
+        half = stol((*lowStr).substr(0, midpoint));
+    }
+    
+    long ID = generateID(half, countDigits(half));
+    while (ID <= upper) {
+        if (isInvalidID(ID, low, upper)) {
+            sum += ID;
+        }
+        half++;
+        ID = generateID(half, countDigits(half));
+    }
+    return sum;
+};
+
+void processFile(ifstream* FILE) {
+    string low;
+    string upper;
+    long long sum = 0;
+    while (!FILE->eof()) {
+        (*FILE) >> low;
+        (*FILE) >> upper;
+        cout << low << ' ' << upper << '\n';
+        sum += countInvalidIDs(&low, &upper);
+    }
+    cout << sum;
+}
 
 int main() {
     ifstream FILE;
@@ -16,18 +66,6 @@ int main() {
     if (FILE.fail()) {
         cout << "ERROR OPENING: " << INPUT << '\n';
     }
-    string foo;
-    string bar;
-    FILE >> foo;
-    FILE >> bar;
-    
-    cout << foo << ' ' << bar << '\n';
-    cout << "SIZES: " << foo.size() << ' ' << bar.size() << '\n';
-    
-    FILE >> foo;
-    FILE >> bar;
-    
-    cout << foo << ' ' << bar << '\n';
-    cout << "SIZES: " << foo.size() << ' ' << bar.size() << '\n';
+    processFile(&FILE);
     FILE.close();
 }
