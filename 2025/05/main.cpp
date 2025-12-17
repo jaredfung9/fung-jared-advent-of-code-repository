bb689f52914acf6c4bbe91f2d4451b02efe260a4
Jared Fung <jaredfung9@gmail.com>
2025-12-17 10:50:09 -0800

    loadRange function && shared_ptr functionality

--- 2025/05/main.cpp
+++ 2025/05/main.cpp
@@ -1,26 +1,41 @@
 #include <iostream>
+#include <memory>
 #include <fstream>
 #include <vector>
 #include <cstdint>
 #include <string>
+
+using std::shared_ptr, std::vector, std::string, std::ifstream, std::cout;
 typedef struct {
     int64_t first;
     int64_t second;
 }Range;
 
-std::vector<Range>* loadRange(std::string* filename) {
-    std::ifstream file;
+typedef shared_ptr<Range> RangePtr;
+
+shared_ptr<vector<RangePtr>> loadRange(string* filename) {
+    ifstream file;
     file.open(*filename);
-    std::vector<Range>* ranges = new std::vector<Range>;
+    auto ranges = std::make_shared<vector<RangePtr>>();
     int64_t a, b;
     while (!file.eof()) {
         file >> a >> b;
-        std::cout << a << ' ' << b << '\n';
+        auto block = std::make_shared<Range>();
+        block->first = a;
+        block->second = b;
+        ranges->push_back(block);
     }
     file.close();
     return ranges;
 }
+
+void printRange(shared_ptr<vector<RangePtr>> ranges) {
+    for (auto it = ranges->begin(); it != ranges->end(); ++it) {
+        cout << it[0]->first << ' '  << it[0]->second << '\n';
+    }
+}
 int main() {
-    std::string input = "inputs/test-ranges.txt";
-    loadRange(&input);
+    string input = "inputs/test-ranges.txt";
+    shared_ptr<vector<RangePtr>> ranges = loadRange(&input);
+    printRange(ranges);
 }
