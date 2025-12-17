21c1b2fc58a976e1e4fac07329557dec8436b1df
Jared Fung <jaredfung9@gmail.com>
2025-12-17 11:06:16 -0800

    implemented part 1 (brute force, shared_ptrs)

--- 2025/05/main.cpp
+++ 2025/05/main.cpp
@@ -28,14 +28,43 @@ shared_ptr<vector<RangePtr>> loadRange(string* filename) {
     file.close();
     return ranges;
 }
+bool freshIngredient(shared_ptr<vector<RangePtr>> ranges, int64_t ingredientID) {
+    for (long unsigned i = 0; i < ranges->size(); i++) {
+        int64_t a, b;
+        a = ((*ranges)[i])->first;
+        b = ((*ranges)[i])->second;
 
+        if ((a<= ingredientID) && (b>=ingredientID)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int countFreshIngredients(shared_ptr<vector<RangePtr>> ranges, string* filename) {
+    ifstream file;
+    file.open(*filename);
+    int64_t query;
+    int count = 0;
+    while (!file.eof()) {
+        file >> query;
+        if (freshIngredient(ranges, query)) {
+            count++;
+        }
+    }
+    file.close();
+    return count;
+}
 void printRange(shared_ptr<vector<RangePtr>> ranges) {
     for (auto it = ranges->begin(); it != ranges->end(); ++it) {
         cout << it[0]->first << ' '  << it[0]->second << '\n';
     }
 }
 int main() {
-    string input = "inputs/test-ranges.txt";
-    shared_ptr<vector<RangePtr>> ranges = loadRange(&input);
-    printRange(ranges);
+    string test_ranges = "inputs/test-ranges.txt";
+    string test_queries = "inputs/test-queries.txt";
+    string ranges = "inputs/ranges.txt";
+    string queries = "inputs/queries.txt";
+    shared_ptr<vector<RangePtr>> rangeVec = loadRange(&ranges);
+    cout << "PART 1: " << countFreshIngredients(rangeVec, &queries) << '\n'; // PART 1: 770
 }
