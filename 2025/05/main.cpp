b4aec52a1b858cd19fd97567243e5fe229d243a1
Jared Fung <jaredfung9@gmail.com>
2025-12-17 10:25:19 -0800

    basic range parsing and load

--- 2025/05/main.cpp
+++ 2025/05/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdint>
+#include <string>
+typedef struct {
+    int64_t first;
+    int64_t second;
+}Range;
 
+std::vector<Range>* loadRange(std::string* filename) {
+    std::ifstream file;
+    file.open(*filename);
+    std::vector<Range>* ranges = new std::vector<Range>;
+    int64_t a, b;
+    while (!file.eof()) {
+        file >> a >> b;
+        std::cout << a << ' ' << b << '\n';
+    }
+    file.close();
+    return ranges;
+}
 int main() {
-    std::cout << "Hello world";
+    std::string input = "inputs/test-ranges.txt";
+    loadRange(&input);
 }
