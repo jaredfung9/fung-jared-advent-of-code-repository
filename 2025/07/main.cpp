5f16bac5bb9ed35ec471673d973591da6891c775
Jared Fung <jaredfung9@gmail.com>
2025-12-18 15:26:40 -0800

    implemented bitwise operators for two char inputs

--- 2025/07/main.cpp
+++ 2025/07/main.cpp
@@ -7,6 +7,14 @@
 char AND(char a, char b) {
     return ((a == '1') && (b == '1')) ? '1' : '0';
 }
+
+char OR(char a, char b) {
+    return ((a == '1') || (b == '1')) ? '1' : '0';
+}
+
+char XOR(char a, char b) {
+    return (((a == '1') || (b == '1'))&&(a != b)) ? '1' : '0';
+}
 int main() {
     std::ifstream FILE;
     FILE.open("inputs/demo.txt");
@@ -16,5 +24,15 @@ int main() {
     std::cout << AND('1', '0');
     std::cout << AND('0', '1');
     std::cout << AND('0', '0');
+    std::cout <<'\n';
+    std::cout << OR('1', '1');
+    std::cout << OR('1', '0');
+    std::cout << OR('0', '1');
+    std::cout << OR('0', '0');
+    std::cout <<'\n';
+    std::cout << XOR('1', '1');
+    std::cout << XOR('1', '0');
+    std::cout << XOR('0', '1');
+    std::cout << XOR('0', '0');
     FILE.close();
 }
