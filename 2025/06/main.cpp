160d268bf4b9569ff9094a18fd8373fd152d94e4
Jared Fung <jaredfung9@gmail.com>
2025-12-18 10:31:11 -0800

    debugging memory_banks not popping

--- 2025/06/main.cpp
+++ 2025/06/main.cpp
@@ -10,10 +10,13 @@
 
 using std::cout, std::ifstream, std::vector, std::stack, std::string, std::istringstream, std::shared_ptr;
 
-string INPUT = "inputs/nums.txt";
-int main() {
-    ifstream FILE;
-    FILE.open(INPUT);
+string NUMINPUT = "inputs/test-nums.txt";
+string OPINPUT = "inputs/test-ops.txt";
+
+void part1() {
+ifstream FILE;
+    // Load terms into memory
+    FILE.open(NUMINPUT);
     vector<shared_ptr<stack<int>>> memory_bank;
     string buffer;
 
@@ -27,11 +30,52 @@ int main() {
             mem_row->push(x);
         }
     }
-
-    shared_ptr<stack<int>> dummy = memory_bank[0];
-    while (!dummy->empty()) {
-        cout << dummy->top() << '\n';
-        dummy->pop();
+    FILE.close();
+    // Process Operations
+    FILE.open(OPINPUT);
+    stack<char> ops;
+    while (!FILE.eof()) {
+        char x;
+        FILE >> x;
+        ops.push(x);
     }
     FILE.close();
+
+    int64_t total = 0;
+    int64_t product = 1;
+    int64_t sum = 1;
+    while (!ops.empty()) {
+        char c = ops.top();
+        ops.pop();
+        switch (c) {
+            case '*':
+                product = 1;
+                for (unsigned long i = 0; i < memory_bank.size(); i++) {
+                    if (!memory_bank[i]->empty()) {
+                        cout << memory_bank[i]->size() << ' ';
+                        product *= memory_bank[i]->top();
+                        memory_bank[i]->pop();
+                    }
+                }
+                cout << "=" << product << '\n';
+                total += product;
+            case '+':
+                sum = 0;
+                for (unsigned long i = 0; i < memory_bank.size(); i++) {
+                    if (!memory_bank[i]->empty()) {
+                        cout << memory_bank[i]->size() << ' ';
+                        sum += memory_bank[i]->top();
+                        memory_bank[i]->pop();
+                    }
+                }
+                cout << "=" << sum << '\n';
+                total += sum;
+        }
+    }
+    
+    cout << total << '\n';
 }
+
+int main() {
+    part1();
+}    
