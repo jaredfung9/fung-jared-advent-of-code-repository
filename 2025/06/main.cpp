cc63db73f09c8f9668340657c06f22eaafc3a209
Jared Fung <jaredfung9@gmail.com>
2025-12-18 11:11:02 -0800

    loadOps

--- 2025/06/main.cpp
+++ 2025/06/main.cpp
@@ -13,26 +13,26 @@ using std::cout, std::ifstream, std::vector, std::stack, std::string, std::istri
 string NUMINPUT = "inputs/test-nums.txt";
 string OPINPUT = "inputs/test-ops.txt";
 
-shared_ptr<stack<string>> loadStackFromLine(string* buffer) {
-    istringstream buffer_stream (*buffer);
+shared_ptr<stack<string>> loadOps(string* filename) {
+    ifstream FILE;
+    FILE.open(*filename);
+    string buffer;
+    std::getline(FILE, buffer);
+    istringstream buffer_stream (buffer);
     auto _stack = std::make_shared<stack<string>>();
     while (!buffer_stream.eof()) {
         string x;
         buffer_stream >> x;
+        cout << x << ' ';
         _stack->push(x);
     }
+    FILE.close();
     return _stack;
 }
 
 void part1() {
-    ifstream FILE;
-    FILE.open(OPINPUT);
-    string buffer;
-    std::getline(FILE,buffer);
-    cout << "INPUT: " << buffer << '\n';
-    shared_ptr<stack<string>> ops = loadStackFromLine(&buffer);
-    cout << ops->size();
-    FILE.close();
+    shared_ptr<stack<string>> ops = loadOps(&OPINPUT);
+    cout << "Loaded Ops: " << ops->size() << '\n';
 }
 
 int main() {
