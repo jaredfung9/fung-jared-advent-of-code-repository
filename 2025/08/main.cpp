ed515e6c482002de56866f68778c6b7a9b82b8a6
Jared Fung <jaredfung9@gmail.com>
2025-12-20 14:12:27 -0800

    Generated distances between points, implemented sorting for Paths

--- 2025/08/main.cpp
+++ 2025/08/main.cpp
@@ -23,8 +23,19 @@ struct Path {
     int id1;
     int id2;
     int64_t distance;
+    Path(int _id1, int _id2, int64_t dist) {
+        id1 = _id1;
+        id2 = _id2;
+        distance = dist;
+    }
 };
-
+bool compPath(Path* a, Path* b) {
+    return a->distance < b->distance;
+}
+class Circuits{
+    vector<int> groupings;
+    int id;
+};
 void Part1() {
     string filename = "inputs/demo.txt";
     ifstream file;
@@ -37,6 +48,25 @@ void Part1() {
         points.push_back(point);
     }
     printf("Points Size: %li\n", points.size());
+
+    vector<Path*> paths;
+    for (uint i = 0; i < points.size(); i++) {
+        for (uint j = i+1; j < points.size(); j++) {
+            Path* path = new Path(i, j, points[i]->distanceTo(points[j]));
+            paths.push_back(path);
+        }
+    }
+    
+    printf("Paths Size: %li\n", paths.size());
+    for (uint i = 0; i < paths.size(); i++) {
+        cout << paths[i]->distance <<'\n';
+    }
+    printf("\nSORTED\n\n");
+    std::sort(paths.begin(),paths.end(), &compPath);
+    for (uint i = 0; i < paths.size(); i++) {
+        printf("%d, %d ", paths[i]->id1, paths[i]->id2);
+        cout << paths[i]->distance<<'\n';
+    }
     file.close();
 }
 int main() {
