e984fce8ec88b45286509a9dd2c51956f6f38af6
Jared Fung <jaredfung9@gmail.com>
2025-12-20 16:37:29 -0800

    Brainstorming day9 pt.2; each row and column has a pair of points associated with it, thinking if it's possible to construct a set of ranges and how to use that set to quickly check if an area is valid

--- 2025/09/main.cpp
+++ 2025/09/main.cpp
@@ -39,8 +39,45 @@ void part1() {
         }
     }
     std::sort(areas.begin(), areas.end(), &intMax);
+    // printf("AREAS: %ld\n", areas.size());
     printf("PART 1: %ld\n", areas[0]); // 4715966250
+}
+/* Brainstorm: It is possible to initialize a 100000x100000 bool map to indicate if a tile is valid. 
+Input list walks us around the perimeter. Maybe we can identify valid ranges for each row and column. 
+Leverage the fact that each line indicates a move (we only move by a column or row) to identify the min and max of each row and column.
+It seems like each row and column has a pair of points.*/
+// void big() {
+//     const int n = 100000;
+//     vector<vector<bool>>rows;
+//     for (int i = 0; i < n; i++) {
+//         vector<bool> row(n, true);
+//         rows.push_back(row);
+//     }
+//     if (rows[96759][38236]) {
+//         printf("finished");
+//     }
+// }
+
+bool compPoint(Point a, Point b) {
+    return a.x < b.x;
+}
+void checkPoints() {
+    ifstream file;
+    file.open("inputs/input.txt");
+    vector<Point> points;
+    int64_t _x, _y;
+    while (file >> _x >> _y) {
+        points.push_back(Point{_x, _y});
+    }
+    file.close();
+    std::sort(points.begin(), points.end(), &compPoint);
+    for (auto iter = points.begin(); iter != points.end(); iter++) {
+        printf("%ld\n", iter->x);
+    }
 }
 int main() {
     part1(); // 4715966250
+    // checkPoints();
+    // big();
+    
 }
