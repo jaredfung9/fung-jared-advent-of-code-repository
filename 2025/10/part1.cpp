fe44ac62c89fe11631b60e92eeae567d326d7d54
Jared Fung <jaredfung9@gmail.com>
2025-12-22 10:33:27 -0800

    day10-pt1 tokenProcessing

--- 2025/10/part1.cpp
+++ 2025/10/part1.cpp
@@ -1,10 +1,52 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-using std::cout, std::string, std::ifstream;
+#include <sstream>
+using std::cout, std::string, std::ifstream, std::stringstream;
 
+/*  Cleans token for processing.
+    [.##.] -> .##.
+    (1,2) -> 1 2
+    {1,2,3} -> 1 2 3
+*/
+string cleanToken(string token) {
+    string clean = token.substr(1, token.size()-2);
+    for (int i = 0; i < (int) clean.size(); i++) {
+        if (clean[i] == ',') {
+            clean[i] = ' ';
+        }
+    }
+    return clean;
+}
+/* Given a token that represents:
+machine-state: [.###.] -> int initial_state
+button: (0,1,2) -> int bitmask
+joltages" {12} -> -1
+*/
 int processToken(string token) {
-    return 0;
+    char c = token[0];
+    string cleanTk = cleanToken(token);
+    int val = 0;
+    switch(c) {
+        case '{':
+            val = -1;  // IE do nothing
+            break;
+        case '[':
+            for (int i = 0; i < (int) cleanTk.size(); i++) {
+                if (cleanTk[i] == '#') {
+                    val += 1 << i;
+                }
+            }
+            break;
+        case '(': 
+            stringstream buttonVals(cleanTk);
+            int x;
+            while (buttonVals >> x) {
+                val += 1 << x;
+            }
+            break;       
+    }
+    return val;
 }
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -14,7 +56,13 @@ int main(int argc, char* argv[]) {
     cout << argv[0] << ' ' << argv[1] << '\n';
     ifstream file;
     file.open(argv[1]);
-    string buffer;
-    while (file>>buffer) { cout << buffer << '\n'; }
+    string token;
+    char c;
+    int x;
+    while (file>>token) { 
+        c = token[0];
+        x = processToken(token);
+        cout << c << ' ' << x << '\n';
+    }
     file.close();
 }
